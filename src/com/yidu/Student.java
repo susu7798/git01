b9e3c152140ee5b44a52a2d3a5dfa0db0af02c60
susu7798 <3626342290@qq.com>
2020-12-24 15:11:04 +0800

    这是我的第一次提交

---
+++ src/com/yidu/Student.java
@@ -0,0 +1,15 @@
+package com.yidu;
+
+/**
+ * @author 罗阳
+ * @version 1.0
+ * @description 一度教育
+ * @date 2020/12/2415:02
+ */
+public class Student {
+
+    public static void main(String[] args) {
+        System.out.println("你好。");
+    }
+
+}
